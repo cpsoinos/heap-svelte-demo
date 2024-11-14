d864d0712e80c8d1401f543a4bfae494f40b7de7
Corey Psoinos <corey.psoinos@contentsquare.com>
2024-11-14 10:46:27 -0500

    set compressCookies to true based on search param

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,69 @@
 <script lang="ts">
 	import Header from './Header.svelte';
 	import '../app.css';
-	
+
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<script type="text/javascript">
+		(window.heapReadyCb = window.heapReadyCb || []),
+			(window.heap = window.heap || []),
+			(heap.load = function (e, t) {
+				(window.heap.envId = e),
+					(window.heap.clientConfig = t = t || {}),
+					(window.heap.clientConfig.shouldFetchServerConfig = !1);
+				var a = document.createElement('script');
+				(a.type = 'text/javascript'),
+					(a.async = !0),
+					(a.src = 'https://cdn.us.heap-api.com/config/' + e + '/heap_config.js');
+				var r = document.getElementsByTagName('script')[0];
+				r.parentNode.insertBefore(a, r);
+				var n = [
+						'init',
+						'startTracking',
+						'stopTracking',
+						'track',
+						'resetIdentity',
+						'identify',
+						'getSessionId',
+						'getUserId',
+						'getIdentity',
+						'addUserProperties',
+						'addEventProperties',
+						'removeEventProperty',
+						'clearEventProperties',
+						'addAccountProperties',
+						'addAdapter',
+						'addTransformer',
+						'addTransformerFn',
+						'onReady',
+						'addPageviewProperties',
+						'removePageviewProperty',
+						'clearPageviewProperties',
+						'trackPageview'
+					],
+					i = function (e) {
+						return function () {
+							var t = Array.prototype.slice.call(arguments, 0);
+							window.heapReadyCb.push({
+								name: e,
+								fn: function () {
+									heap[e] && heap[e].apply(heap, t);
+								}
+							});
+						};
+					};
+				for (var p = 0; p < n.length; p++) heap[n[p]] = i(n[p]);
+			});
+
+		const searchParams = new URLSearchParams(window.location.search);
+		const shouldCompressCookies = searchParams.get('compressCookies') === 'true';
+		heap.load('660636389', {
+			compressCookies: shouldCompressCookies
+		});
+	</script>
+</svelte:head>
 
 <div class="app">
 	<Header />
